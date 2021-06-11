0ff057389939934850853641d6b75bd1243fd6f1
evangart <evangart.v@gmail.com>
2021-06-11 16:29:34 +0300

    Fix refresh token issues

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,28 @@ const Tokens = require("../services/tokens")
 const db = new DB()
 const tokens = new Tokens()
 
+const accessCookieOpt = { maxAge: 10 * 60000, httpOnly: true, sameSite: true }
+const refreshCookieOpt = { maxAge: 24 * 3600000, httpOnly: true, sameSite: true }
+
 router.post("/signin", async (req, res) => {
+  const cookies = req.cookies
   const { email, password } = req.body
+
   const hash = crypto.createHash("sha256")
   hash.update(password)
+
+  try {
+    const { refreshToken } = cookies
+
+    if (refreshToken) {
+      const { user } = await tokens.decryptRefresh(refreshToken)
+      const activeToken = await db.tokens.get(user.id, refreshToken)
+      if (activeToken) return res.json({ message: "User already signed in" })
+    }
+  } catch (err) {
+    console.error(err)
+    res.status(400).json({ error: "Internal error" })
+  }
 
   try {
     const user = await db.users.get(email)
@@ -25,14 +43,13 @@ router.post("/signin", async (req, res) => {
 
     const accessToken = await tokens.genAccess(user.id, user.email)
     const refreshToken = await tokens.genRefresh(user.id, user.email)
-    const cookieOpt = { maxAge: 24 * 60 * 60, httpOnly: true }
 
     await db.tokens.add(user.id, refreshToken)
 
     res
       .status(201)
-      .cookie("accessToken", accessToken, cookieOpt)
-      .cookie("refreshToken", refreshToken, cookieOpt)
+      .cookie("accessToken", accessToken, accessCookieOpt)
+      .cookie("refreshToken", refreshToken, refreshCookieOpt)
       .json({ message: "Successfully sign in" })
       .end()
   } catch (err) {
@@ -51,15 +68,16 @@ router.post("/signin/new_token", async (req, res) => {
     const { user } = await tokens.decryptRefresh(refreshToken)
     userId = user.id
 
-    const accessToken = await tokens.regenAccess(refreshToken)
-    const cookieOpt = { maxAge: 24 * 60 * 60, httpOnly: true }
+    const session = await db.tokens.get(userId, refreshToken)
+    if (!session) return res.status(401).json({ error: "Token expired" })
 
+    const accessToken = await tokens.regenAccess(refreshToken)
     if (!accessToken) return res.status(403).json({ error: "Invalid token" })
 
     res
       .status(201)
-      .cookie("accessToken", accessToken, cookieOpt)
-      .cookie("refreshToken", refreshToken, cookieOpt)
+      .cookie("accessToken", accessToken, accessCookieOpt)
+      .cookie("refreshToken", refreshToken, refreshCookieOpt)
       .json({ message: "Token was refreshed successfully" })
       .end()
   } catch (err) {
@@ -87,12 +105,11 @@ router.post("/signup", async (req, res) => {
 
     const accessToken = await tokens.genAccess(userId, email)
     const refreshToken = await tokens.genRefresh(userId, email)
-    const cookieOpt = { maxAge: 24 * 60 * 60, httpOnly: true }
 
     res
       .status(201)
-      .cookie("accessToken", accessToken, cookieOpt)
-      .cookie("refreshToken", refreshToken, cookieOpt)
+      .cookie("accessToken", accessToken, accessCookieOpt)
+      .cookie("refreshToken", refreshToken, refreshCookieOpt)
       .json({ message: "Successfully sign up" })
       .end()
   } catch (err) {
@@ -123,7 +140,10 @@ router.get("/logout", (req, res) => {
   tokens.decryptRefresh(refreshToken)
     .then(({ user }) => db.tokens.delete(user.id, refreshToken))
     .then(() => res.status(201).json({ message: "Successfully logout" }))
-    .catch((err) => res.status(400).json({ error: "Internal error" }))
+    .catch((err) => {
+      console.error(err)
+      res.status(400).json({ error: "Internal error" })
+    })
 })
 
 module.exports = router
